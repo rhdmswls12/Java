ca3f5b2347c089fb17fdc5a72a586a3886ae5cb6
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-10 16:28:44 +0900

    ex2-6

--- Study/src/CarTest.java
+++ Study/src/CarTest.java
@@ -2,23 +2,10 @@ class Car{
 	private String color;
 	private int speed;
 	private int gear;
-	
-	//첫번째 생성자
-	public Car(String c,int s, int g) {
-		color=c;
-		speed=s;
-		gear=g;
-	}
-	//두번째 생성자
-	public Car() {
-		color="red";
-		speed=0;
-		gear=1;
-	}
+	//생성자를 선언하지 않았으므로 컴파일러가 자동으로 디폴트 생성자를 만든다.
 }
 public class CarTest{
 	public static void main(String args[]) {
-		Car c1=new Car("blue",100,0);
-		Car c2=new Car();
+		Car c1=new Car(); //디폴트 생성자 호출
 	}
 }
