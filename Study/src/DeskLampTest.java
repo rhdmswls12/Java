051abb0db6238690d5c27f0694aee7643bf4ee64
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-10 16:04:46 +0900

    ex2-4

---
+++ Study/src/DeskLampTest.java
@@ -0,0 +1,21 @@
+class DeskLamp{
+	private boolean isOn;
+	public void turnOn() {
+		isOn=true;
+	}
+	public void turnOff() {
+		isOn=false;
+	}
+	public String toString() {
+		return "현재 상태는 "+(isOn==true?"켜짐":"꺼짐");
+	}
+}
+public class DeskLampTest {
+
+	public static void main(String[] args) {
+		DeskLamp myLamp=new DeskLamp();
+		myLamp.turnOn();
+		System.out.print(myLamp);
+	}
+
+}
