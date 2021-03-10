cbe1321ae0ac84f00f32f5ad25741c35eb5dc641
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-10 15:43:35 +0900

    ex2-3

--- Study/src/CarTest.java
+++ Study/src/CarTest.java
@@ -1,22 +1,38 @@
 class Car{
 	//필드 정의
-	String color;
-	int speed;
-	int gear;
-	void print() {
-		System.out.println("( "+color+", "+speed+", "+gear+" )");
+	private String color;
+	private int speed;
+	private int gear;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String c) {
+		color=c;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int s) {
+		speed=s;
+	}
+	public int getGear() {
+		return gear;
+	}
+	public void setGear(int g) {
+		gear=g;
 	}
 }
-
 public class CarTest {
 
 	public static void main(String[] args) {
-		Car myCar=new Car(); //객체 생성
+		Car myCar=new Car();   //객체 생성
 		
-		myCar.color="red";   //객체의 필드 변경
-		myCar.speed=0;
-		myCar.gear=1;
-		myCar.print();       //객체의 메소드 호출
+		myCar.setColor("red");       //설정자 메소드 호출
+		myCar.setSpeed(100);
+		myCar.setGear(1);
+		
+		System.out.println("현재 자동차의 색상은 "+myCar.getColor());  //접근자 메소드 호출
+		System.out.println("현재 자동차의 속도는 "+myCar.getSpeed());
+		System.out.println("현재 자동차의 기어는 "+myCar.getGear());
 	}
-
 }
