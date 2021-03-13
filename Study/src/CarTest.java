ec17d0db14de479463dd8c491e256073deeca650
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-13 18:38:01 +0900

    instance example

--- Study/src/CarTest.java
+++ Study/src/CarTest.java
@@ -2,10 +2,15 @@ class Car{
 	private String color;
 	private int speed;
 	private int gear;
-	//생성자를 선언하지 않았으므로 컴파일러가 자동으로 디폴트 생성자를 만든다.
+
+	public Car(String c,int s, int g) {
+		color=c;
+		speed=s;
+		gear=g;
+	}
 }
 public class CarTest{
 	public static void main(String args[]) {
-		Car c1=new Car(); //디폴트 생성자 호출
+		 //디폴트 생성자 호출
 	}
 }
