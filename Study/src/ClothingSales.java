3a76a0e754acea6c9e8bf624529509efdd49a95a
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-15 17:50:05 +0900

    scope example

--- Study/src/ClothingSales.java
+++ Study/src/ClothingSales.java
@@ -1,24 +1,25 @@
-class Cloth{
+class Clothes{
 	private String color;
 	private int size;
 	private int id;
 	private static int numberOfClothes=0;  //정적 변수
-	
-	public Cloth(String c,int s) { //생성자
+
+	public Clothes(String c,int s) {  //생성자
 		color=c;
 		size=s;
 		id=++numberOfClothes;
 	}
 	
-	public static int getNumberOfClothes() { //정적 메소드
+	public static int getNumberOfClothes() {  //정적 메소드
 		return numberOfClothes;
 	}
 }
 public class ClothingSales{
 	public static void main(String args[]) {
-		 Cloth c1=new Cloth("blue",100);
-		 Cloth c2=new Cloth("white",0);
-		 int n=Cloth.getNumberOfClothes(); //정적 메소드이므로 클래스 이름을 통해 호출
-		 System.out.println("지금까지 생성된 옷 개수= "+n);
+		Clothes c1=new Clothes("blue",66);
+		Clothes c2=new Clothes("white",55);
+		int n=Clothes.getNumberOfClothes();  //정적 메소드이므로 클래스 이름 통해 호출
+		System.out.println("지금까지 생성된 옷 개수= "+n);
 	}
 }
+
