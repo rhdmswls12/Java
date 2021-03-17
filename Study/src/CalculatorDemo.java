e6bb586b98c8c77da3ef1dc98b5a9fc7027094fc
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-17 15:03:37 +0900

    try catch

--- Study/src/CalculatorDemo.java
+++ Study/src/CalculatorDemo.java
@@ -5,23 +5,26 @@ class Calculator1{
 		this.left=left;
 		this.right=right;
 	}
-	public void sum() {
-		System.out.println(this.left+this.right);
-	}
-	public void avg() {
-		System.out.println((this.left+this.right)/2);
-	}
-	public String toString() {
-		return "left: "+this.left+", right: "+this.right;
+	public void divide() {
+		try {
+			System.out.print("계산결과는 ");
+			System.out.print(this.left/this.right);
+			System.out.print(" 입니다.");
+		}catch(Exception e) {
+			System.out.println("오류가 발생했습니다: "+e.getMessage());
+		}
+		
 	}
 }
 public class CalculatorDemo {
-
 	public static void main(String[] args) {
 		Calculator1 c1=new Calculator1();
-		c1.setOprands(10, 20);
-		System.out.println(c1);
-		System.out.println(c1.toString());
+		c1.setOprands(10, 0);
+		c1.divide();
+		
+		Calculator1 c2=new Calculator1();
+		c2.setOprands(10, 5);
+		c2.divide();
 	}
 
 }
