3c8393d84a51d5c454e54a217b447216650da88b
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-20 22:35:18 +0900

    clone()

--- Study/src/ObjectDemo.java
+++ Study/src/ObjectDemo.java
@@ -3,18 +3,21 @@ class Student{
 	Student(String name){
 		this.name=name;
 	}
-	public boolean equals(Object obj) {
-		Student _obj=(Student)obj;
-		return name==_obj.name;
+	public Object clone() throws CloneNotSupportedException{
+		return super.clone();
 	}
 }
 public class ObjectDemo {
 
 	public static void main(String[] args) {
 		Student s1=new Student("egoing");
-		Student s2=new Student("egoing");
-		System.out.println(s1==s2);
-		System.out.println(s1.equals(s2));
+		try {
+			Student s2=(Student)s1.clone();
+			System.out.println(s1.name);
+			System.out.println(s2.name);
+		}catch(CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
 
 	}
 
