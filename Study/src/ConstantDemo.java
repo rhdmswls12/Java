51227cc887b1a67546ec5ec531d591134d345233
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-22 18:51:11 +0900

    final static

--- Study/src/ConstantDemo.java
+++ Study/src/ConstantDemo.java
@@ -1,9 +1,20 @@
-
 public class ConstantDemo {
-
+	private final static int APPLE=1;
+	private final static int PEACH=2;
+	private final static int BANANA=3;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int type=APPLE;
+		switch(type) {
+		case APPLE:
+			System.out.println(57);
+			break;
+		case PEACH:
+			System.out.println(34);
+			break;
+		case BANANA:
+			System.out.println(93);
+			break;
+		}
 	}
 
 }
