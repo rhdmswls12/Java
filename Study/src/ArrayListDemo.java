bda181acd97fcc4d05b575e331819702d12dd329
rhdmswls12 <rhgusdnr1293@naver.com>
2021-03-25 19:22:20 +0900

    testtest

--- Study/src/ArrayListDemo.java
+++ Study/src/ArrayListDemo.java
@@ -15,7 +15,7 @@ public class ArrayListDemo {
 		al.add("three");
 		for(int i=0;i<al.size();i++) {
 			System.out.println(al.get(i));
-			System.out.println("testtesttest");
+			System.out.println("testtesttesttest");
 		}
 
 	}
